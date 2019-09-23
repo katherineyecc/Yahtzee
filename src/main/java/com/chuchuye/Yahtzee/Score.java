042f879e3ba99ab8f62a248858aa2200d4577f5c
katherineyecc <katherineyecc@163.com>
2019-09-23 10:04:16 -0400

    make the ScoreTest success.

---
+++ src/main/java/com/chuchuye/Yahtzee/Score.java
@@ -0,0 +1,15 @@
+package com.chuchuye.Yahtzee;
+
+public class Score {
+	
+	private int currentScore;
+	
+	public int getScore() {
+		return currentScore;
+	}
+	
+	public void setScore(int score) {
+		currentScore = score;
+	}
+
+}
